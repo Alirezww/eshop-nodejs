76ed36b8e57d95534160634e901ab61a468aecbb
Alireza <alirezatlb45@gmail.com>
2022-09-04 02:30:16 +0430

    added schema for blog mdoel

--- app/models/Blog.js
+++ app/models/Blog.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const BlogSchema = new mongoose.Schema({
-
+    author : { type : mongoose.Types.ObjectId, required : true },
+    title : { type : String, required : true, trim : true },
+    image : { type : String, required : true, trim : true },
+    tags : { type : [String], default : []},
+    category : { type : mongoose.Types.ObjectId, required : true },
+    comments : { type : [], default : [] },
+    likes : { type : mongoose.Types.ObjectId, default : []},
+    dislikes : { type : mongoose.Types.ObjectId, default : []},
+    bookmark : { type : mongoose.Types.ObjectId, default : []}
 })
 
 const BlogModel = mongoose.model("Blog", BlogSchema);
