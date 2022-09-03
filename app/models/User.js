596c9cd84fca107db84543d0c34668785b0abbe9
Alireza <alirezatlb45@gmail.com>
2022-09-04 02:53:31 +0430

    added schema for user mdoel

--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
-const Schema = new mongoose.Schema({
-
+const userSchema = new mongoose.Schema({
+    first_name : {type : String , trim : true},
+    last_name : {type : String, trim : true},
+    username : {type : String , required : true, unique : true, trim : true},
+    mobile : {type : String , required : true, unique : true},
+    email : {type : String , required : true, unique : true, trim : true},
+    password : {type : String , required : true},
+    otp : { type : Object, default : {
+        code : "",
+        expires : 0
+    }},
+    bills : { type : [], default : [] },
+    roles : {type : [String] , default : ["USER"]},
+    discount : { type : Number, default : 0 },
+    birthday : { type : String }
 })
 
-const Model = mongoose.model("", Schema);
+const UserModel = mongoose.model("User", userSchema);
 
-module.exports = Model
+module.exports = UserModel
