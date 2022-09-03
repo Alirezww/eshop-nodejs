056904b28c6579ee02aa783b8ea6dee1f0ef434e
Alireza <alirezatlb45@gmail.com>
2022-09-04 02:46:13 +0430

    added schema for user mdoel

--- app/models/Slider.js
+++ app/models/Slider.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const SliderSchema = new mongoose.Schema({
-
+    title : { type : String, trim : true },
+    text : { type : String, trim : true },
+    image : { type : String, required : true, trim : true },
+    type : { type : String, default : "main" }
 })
 
-const SliderModel = mongoose.model("", SliderSchema);
+const SliderModel = mongoose.model("Slider", SliderSchema);
 
 module.exports = SliderModel
