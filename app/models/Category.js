9d0547fe1fead3d563b285667c3ec746dfd2a697
Alireza <alirezatlb45@gmail.com>
2022-09-04 02:31:12 +0430

    added schema for category mdoel

--- app/models/Category.js
+++ app/models/Category.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-
+    title : { type : String, required : true, trim : true }
 })
 
-const CategoryModel = mongoose.model("", CategorySchema);
+const CategoryModel = mongoose.model("Category", CategorySchema);
 
 module.exports = CategoryModel
