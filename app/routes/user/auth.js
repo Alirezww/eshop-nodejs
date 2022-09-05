1c2b7bbacf0c0bcd2fd9bddcc3ecd3a501f23185
Alireza <alirezatlb45@gmail.com>
2022-09-05 20:08:18 +0430

    using swagger for login route

--- app/routes/user/auth.js
+++ app/routes/user/auth.js
@@ -2,6 +2,28 @@ const { UserAuthController } = require("../../http/controllers/user/auth/auth.co
 
 const router = require("express").Router();
 
+/**
+ * @swagger
+ * /user/login:
+ *      post:
+ *          summary: login user in userpanel with phone number
+ *          description: one time password (OTP)
+ *          parameters:
+ *          -   name: mobile
+ *              description: IRI-fa phone number
+ *              in: formData
+ *              type: string
+ *              required: true
+ *          responses:
+ *              201:
+ *                  description : success
+ *              400:
+ *                  description : Bad Request
+ *              401:
+ *                  description : Unauthorization
+ *              500:
+ *                  description : Internal Server Error
+ */
 router.post("/login", UserAuthController.login);
 
 module.exports = {
