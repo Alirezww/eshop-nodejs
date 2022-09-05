5ba9061522823b5ca54ad6a7238f49dae6a35700
Alireza <alirezatlb45@gmail.com>
2022-09-05 21:50:03 +0430

    added genrator access token

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -6,6 +6,27 @@ const randomNumberGenerator = () => {
     return Math.floor((Math.random() * 90000) + 10000)
 }
 
+const SignAccessToken = (userID) => {
+    return new Promise(async (resolve, reject) => {
+        const user = await UserModel.findById(userID);
+
+        const payload = {
+            mobile : user.mobile,
+            userID : user._id 
+        };
+        const secret = "b042b83352360176a5e913215113f910";
+        const options = {
+            expiresIn : "1h"
+        };
+
+        JWT.sign(payload, secret, options, (err, token) => {
+            if(err) reject(createHttpError.InternalServerError("خطای سرور پیش آمده است لطفا دوباره بعدا امتحان کنید."));
+            resolve(token)
+        })
+    })
+}
+
 module.exports = {
     randomNumberGenerator,
+    SignAccessToken
 }
