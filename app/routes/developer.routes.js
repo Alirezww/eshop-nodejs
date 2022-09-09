205b08926c87f49b02e595e74a0590579d929761
Alireza <alirezatlb45@gmail.com>
2022-09-09 17:14:22 +0430

    added swagger for hash string route

--- app/routes/developer.routes.js
+++ app/routes/developer.routes.js
@@ -1,5 +1,28 @@
 const router = require("express").Router();
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
+
+/**
+ * @swagger
+ * tags:
+ *      name: DeveloperRoutes
+ *      description: developer utils
+ */
+
+/**
+ * @swagger
+ * /developer/password-hash/(password):
+ *      get:
+ *          tags: [DeveloperRoutes]
+ *          summary: hash data with bcrypt
+ *          parameters:
+ *              -   in: path
+ *                  type: string
+ *                  name: password
+ *                  required: true    
+ *          responses:
+ *              200:
+ *                  description: success   
+ */       
 
 router.get("/password-hash/:password", (req, res, next) => {
     const password = req.params.password;
