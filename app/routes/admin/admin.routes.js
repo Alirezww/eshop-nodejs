3a03bc9baf0d351b5b36c9c5bbc91eeacb80f4ec
Alireza <alirezatlb45@gmail.com>
2022-09-10 14:45:01 +0430

    added admin swagger tags

--- app/routes/admin/admin.routes.js
+++ app/routes/admin/admin.routes.js
@@ -2,6 +2,13 @@ const { CategoryRoutes } = require("./category");
 
 const router = require("express").Router();
 
+/**
+ * @swagger
+ * tags:
+ *      name: Admin-Panel
+ *      description: actions of admin (add, edit, remove, read)
+ */
+
 router.use("/category", CategoryRoutes)
 
 module.exports = {
