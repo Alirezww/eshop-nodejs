625d765770afc4f7491a4a1ec1200bf5cf09f2a0
Alireza <alirezatlb45@gmail.com>
2022-09-10 14:15:34 +0430

    added filed parent to category model

--- app/models/Category.js
+++ app/models/Category.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-    title : { type : String, required : true, trim : true }
+    title : { type : String, required : true, trim : true },
+    parent : { type: mongoose.Types.ObjectId, default: undefined }
 })
 
 const CategoryModel = mongoose.model("Category", CategorySchema);
