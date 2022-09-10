597a9fd05c58c7fd3630ae8a7326cebfdbdff9ca
Alireza <alirezatlb45@gmail.com>
2022-09-10 16:08:44 +0430

    added mongoId pattern regex for category validation

--- app/http/validators/admin/category.schema.js
+++ app/http/validators/admin/category.schema.js
@@ -1,8 +1,9 @@
 const Joi = require("@hapi/joi");
+const { MongoIDPattern } = require("../../../utils/constans");
 
 const addCategorySchema = Joi.object({
     title : Joi.string().min(3).max(30).error(new Error("عنوان دسته بندی صحیح نمی باشد.")),
-    parent: Joi.string().allow("").allow('').error(new Error("شناسه ارسال شده صحیح نمی باشد."))
+    parent: Joi.string().pattern(MongoIDPattern).allow("").allow('').error(new Error("شناسه ارسال شده صحیح نمی باشد."))
 });
 
 module.exports = {
