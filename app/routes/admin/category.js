44eef9bd907db0bc8c9810d1abea01ee37a64e7a
Alireza <alirezatlb45@gmail.com>
2022-09-10 14:41:26 +0430

    added admin routes

--- app/routes/admin/category.js
+++ app/routes/admin/category.js
@@ -2,6 +2,27 @@ const { CategoryController } = require("../../http/controllers/admin/category.co
 
 const router = require("express").Router();
 
+/**
+ * @swagger
+ * /admin/category/add:
+ *      post:
+ *          summary: create new category
+ *          tags: [Admin-Panel]
+ *          parameters:
+ *              -   in: formData
+ *                  name: title
+ *                  type: string
+ *                  required: true
+ * 
+ *              -   in: formData
+ *                  name: parent
+ * 
+ *          responses:
+ *              201:
+ *                  description: success
+ *              500:
+ *                  description: InternalServerError
+ */
 router.post("/add", CategoryController.addCategory)
 
 module.exports = {
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,11 +1,13 @@
+const { AdminRoutes } = require("./admin/admin.routes");
 const { HomeRoutes } = require("./api");
 const { DeveloperRoutes } = require("./developer.routes");
 const { userAuthRoutes } = require("./user/auth");
 
 const router = require("express").Router();
 
+router.use("/admin", AdminRoutes)
 router.use("/user", userAuthRoutes);
-router.use("/developer", DeveloperRoutes)
+router.use("/developer", DeveloperRoutes);
 router.use("/", HomeRoutes);
 
 module.exports = {
