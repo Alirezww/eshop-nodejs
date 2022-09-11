3b1016ea9f08a149e881c6780b98fe5fd9a5fca6
Alireza <alirezatlb45@gmail.com>
2022-09-11 13:36:52 +0430

    added query for creating new namespace

--- app/http/controllers/support/namesapces.controller.js
+++ app/http/controllers/support/namesapces.controller.js
@@ -1,15 +1,27 @@
+const createHttpError = require("http-errors");
+const { ConversationModel } = require("../../../models/conversation");
 const Controller = require("./../controller");
 
-class NameSpaceController extends Controller{
-    addNemeSpace(req, res, next){
-        try{
+class NamespaceController extends Controller{
 
+    async addNemespace(req, res, next){
+        try{
+            const { title, endpoint } = req.body;
+            const conversation = await ConversationModel.create({ title, endpoint });
+            if(!conversation) throw createHttpError.InternalServerError("خطایی سمت سرور رخ داده است. لطفا بعدا امتحان کنید.");
+            return res.status(200).json({
+                data: {
+                    statusCode: 200,
+                    message: "فضای نام جدیدی با موفقیت ایجاد شده است."
+                }
+            });
         }catch(err){
             next(err);
         }
     }
+
 }
 
 module.exports = {
-    NameSpaceController : new NameSpaceController()
+    NamespaceController : new NamespaceController()
 }
