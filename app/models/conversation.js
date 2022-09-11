af170c97d0ce45e92de4e4ae63bc5012ea11f122
Alireza <alirezatlb45@gmail.com>
2022-09-11 13:29:02 +0430

    added schemas for chatroom

---
+++ app/models/conversation.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+    sender: { type: mongoose.Types.ObjectId, ref: "User" },
+    message: { type: String },
+    dateTime: { type: String }
+})
+
+const roomSchema = new mongoose.Schema({
+    name : { type: String },
+    description: { type: String },
+    image: { type: String },
+    messages: { type: [messageSchema], default: [] }
+})
+
+const conversationSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    endpoint: { type: String, required: true },
+    rooms: { type: [roomSchema], default: [] }
+});
+
+module.exports = {
+    ConversationModel : mongoose.model("Conversation", conversationSchema)
+}
